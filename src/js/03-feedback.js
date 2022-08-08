3338cfa274a1563aa87d61cba389e7ffbbb561c0
Artem Karpenko <dante120591@gmail.com>
2022-08-08 23:44:49 +0300

    Update task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,40 @@
 var throttle = require('lodash.throttle');
 
-const textInput = document.querySelector('.feedback-form');
-const submitBtn = document.querySelector('.feedback-form button');
+const form = document.querySelector('.feedback-form');
 const emailInput = document.querySelector('.feedback-form input');
 const messageInput = document.querySelector('.feedback-form textarea');
 const formData = {};
 
 formFill();
 
-textInput.addEventListener('input', throttle(event => {
+form.addEventListener('input', throttle(event => {
   formData[event.target.name] = event.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }, 500));
 
+form.addEventListener('submit', onFormSubmit);
+
+
 function formFill() {
   const formString = localStorage.getItem('feedback-form-state');
   const formJs = JSON.parse(formString);
+
+  
   if (formJs) {
     emailInput.value = formJs.email;
     messageInput.value = formJs.message;
-  } 
+  }
 }
 
-submitBtn.addEventListener('click', onSubmitClick);
 
-function onSubmitClick(evt) {
+
+function clearStorage() {
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+}
+
+function onFormSubmit(evt) {
   evt.preventDefault();
   if (!emailInput.value || !messageInput.value) {
     return;
@@ -33,8 +43,4 @@ function onSubmitClick(evt) {
   clearStorage();
 }
 
-function clearStorage() {
-  localStorage.removeItem('feedback-form-state');
-  emailInput.value = '';
-  messageInput.value = '';
-}
+
